9a10f7abaf3c595c16930d5bb0399d752851c377
alijianzhang <zhangyl1998@126.com>
2024-01-12 16:45:14 +0800

    update

--- textProgram/vite.config.js
+++ textProgram/vite.config.js
@@ -3,8 +3,11 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./text",
+  base: "/text",
   plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
