263204c0edd94785a415184b5598ca90b69a6223
alijianzhang <zhangyl1998@126.com>
2024-01-12 16:54:50 +0800

    test

--- textProgram/vite.config.js
+++ textProgram/vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/threejs-example",
+  base: "/threejs-example/text",
   plugins: [vue()],
   server: {
     host: "0.0.0.0",
